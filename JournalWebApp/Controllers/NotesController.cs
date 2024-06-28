2ebb071f03548acf70cadf8dfde7612b8b027972
yahya-t <yahya.tlr99@gmail.com>
2024-06-28 13:10:48 +0100

    Add project files.

---
+++ JournalWebApp/Controllers/NotesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JournalWebApp.Controllers
+{
+    public class NotesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
