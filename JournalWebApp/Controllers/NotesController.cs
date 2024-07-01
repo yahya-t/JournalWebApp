358779b1de79d2a4346f329c9051423d5f2e4c4d
yahya-t <yahya.tlr99@gmail.com>
2024-07-01 10:20:40 +0100

    Set up initial model and controller classes for Notes

--- JournalWebApp/Controllers/NotesController.cs
+++ JournalWebApp/Controllers/NotesController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JournalWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JournalWebApp.Controllers
 {
     public class NotesController : Controller
     {
+        public List<NotesModel> Notes { get; set; }
+
+        public NotesController()
+        {
+            Notes = SampleNotes();
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(Notes);
         }
+
+        // Return a sample list of notes
+        private List<NotesModel> SampleNotes()
+        {
+            return new List<NotesModel>
+            {
+                new NotesModel
+                {
+                    Id = 1,
+                    CreationDate = DateTime.Now,
+                    Title = "This is an example of a note you can create!",
+                    Content = "This is a web application designed for you to create notes and increase you productivity...",
+                    IsActive = true
+                },
+                new NotesModel
+                {
+                    Id = 2,
+                    CreationDate = DateTime.Now.AddDays(-1),
+                    Title = "Another example note",
+                    Content = "Notes can be deactivated if you don't need them, but you also don't want to delete them",
+                    IsActive = false
+                }
+            };
+        }
+
     }
 }
---
+++ JournalWebApp/Models/NotesModel.cs
@@ -0,0 +1,11 @@
+﻿namespace JournalWebApp.Models
+{
+    public class NotesModel
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
